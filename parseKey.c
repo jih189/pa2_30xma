a9f7d09cc07f7e50b114793b4fbf1afe83d2397e
Liu Tan <litan@ucsd.edu>
2015-04-26 20:34:44 -0700

    add description of parseKey.c

---
+++ parseKey.c
@@ -0,0 +1,32 @@
+
+/*
+ * Filename:    parseKey.c
+ * Author:      Liu Tan
+ * Userid:      cs30xma
+ * Description: C routine to convert a string to a unsigned long.
+ * Date:        4/26/2015
+ * Sources of Help: none 
+ */
+
+#include <stdio.h>
+#include "pa2_strings.h"
+#include "pa2.h"
+
+/*
+ * Function name: parseKey
+ * Function prototype:  int parseKey( char *str, unsigned long *key )
+ * Description: 
+ *        This module is responsible for parsing either of the key values.
+ * Parameters:
+ *        arg1: char *str --- the string to be converted
+ *        arg2: unsigned long *key --- the unsigned long to store value.
+ * Side Effects: it will set key.
+ * Error Conditions: Checks if argl1 is valid string of interger.
+ * Return Value: 
+ *        0 -- if the string is parsed successfully.
+ *        error number -- if any error occur.
+ */
+
+int parseKey( char *str, unsigned long *key ) {
+  
+}
